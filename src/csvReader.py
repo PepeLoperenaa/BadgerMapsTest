a77bb850cccd28d4c38f384fa9aa759cd8e0db39
PepeLoperenaa <pepeloperena@gmail.com>
2022-11-14 18:09:34 +0100

    Added base application files.
    With this also added the csv file which will be used for testing as well as the .gitignore to stop unnecessary files to be uploaded

---
+++ src/csvReader.py
@@ -0,0 +1,24 @@
+
+
+def earliest_check_in_date():
+    print("base")
+
+
+def lastest_check_in_date():
+    print("base")
+
+
+def customer_full_names():
+    print("base")
+
+
+def companies_users_jobs():
+    print("base")
+
+# Need to check that every csv file contains  Street, Zip, City, Last Check-in Date and CompanyCheck
+
+'''
+It is recommended to log exceptions in case that a required field is empty for that row but the rest of the file is still processed.
+It is recommended to log an exception in case a row contains less fields than expected but the rest of the file is still processed.
+It is recommended to log an exception in case a row does not contain any data but the rest of the file is still processed.
+'''
