4487835ed1781800095b68fadae5a3cd31e07505
PepeLoperenaa <pepeloperena@gmail.com>
2022-11-17 15:29:06 +0100

    Added functionality
    Added a new method + added functionality to the datetime functions. These find the earliest check in date as well as the lastest check in date.

--- src/csvReader.py
+++ src/csvReader.py
@@ -1,13 +1,10 @@
 import csv
-from datetime import datetime
-from dateutil.rrule import rrule, DAILY
-import re
+import datetime as dt
 
-filename = "../files/testFile.csv"
 rows = []
 
 
-def open_csv2(file_name):
+def open_csv(file_name):
     with open(file_name, encoding="UTF-8") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         header = next(reader)
@@ -32,57 +29,50 @@ def open_csv2(file_name):
                     print("Missing Company")
                 elif cell == ' ' and flag:
                     return False
-        print(rows)
     return True
 
 
+def convert_date_time():
+    dates = []  # get dates in string format
+    dates_list = [dt.datetime]  # datatype is datetime
+    for row in rows:
+        dates.append(row[6])
+    full_dates = [x for x in dates if x != '']  # delete records that are empty
+    return [dt.datetime.strptime(date, '%d/%m/%Y').date() for date in full_dates]
+
 
 def earliest_check_in_date():  # Iterate through list and get the earliest date
-    for row in rows:
-        if re.match(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}', row):
-            print(row, 'is a date')
-
-    '''
-    number = 0
-    for row in rows:
-        date = datetime.strptime(rows[number][6], '%d/%m/%Y')  # works but cannot iterate on it.
-        number += 1
-        for d in rrule(DAILY, dtstart=date, until=date):
-            print(d.strftime('%d/%m/%Y'))
-    '''
+    min_list = convert_date_time()
+    print(min(min_list))
 
 
 def lastest_check_in_date():  # Iterate through list and get the latest date
-    number = 0
-    for row in rows:
-        print(rows[number][6])
-        number += 1
+    max_list = convert_date_time()
+    print(max(max_list))
 
 
 def customer_full_names():  # Iterate from the whole list and concatenate value[0][1] in every record.
-    number = 0
     rows.sort()  # Sort the list alphabetically.
     for row in rows:
-        fname = rows[number][0]
-        lname = rows[number][1]
-        number += 1
+        fname = row[0]
+        lname = row[1]
         full_name = fname + " " + lname
         print(full_name)
 
 
 def companies_users_jobs():  # get jobs from the clients. Need to sort alphabetically.
-    number = 0
-    rows[7].sort()
+    order = []
     for row in rows:
-        print(rows[number][7])
-        number += 1
+        order.append(row[7])
+    order.sort()
+    print(order)
 
 
-open_csv2(filename)
-# customer_full_names()
-# companies_users_jobs()
-# earliest_check_in_date()
-
+open_csv("../files/testFile.csv")
+earliest_check_in_date()
+lastest_check_in_date()
+customer_full_names()
+companies_users_jobs()
 
 # Need to check that every csv file contains  Street, Zip, City, Last Check-in Date and CompanyCheck
 
